6e1ba9098fc5060f506fab04916401046c5a8db5
iida.nao08@gmail.com <genome@NCCT20190221C.local>
2020-07-14 08:07:19 +0900

    use dict

--- add_bam_haptag.py
+++ add_bam_haptag.py
@@ -12,58 +12,59 @@ from argparse import ArgumentParser
 import pysam
 import time
 start_time = time.time()
+run_time = (time.time()-start_time)/60
 
         
 def add_haplotypes(hap_bampath, me_bampath, out_bampath) :
     
-    hap1_1_qnames = list()
-    hap1_2_qnames = list()
-    hap1_3_qnames = list()
-    hap1_4_qnames = list()
-    hap1_5_qnames = list()
-    hap1_6_qnames = list()
-    hap1_7_qnames = list()
-    hap1_8_qnames = list()
-    hap1_9_qnames = list()
-    hap1_10_qnames = list()
-    hap1_11_qnames = list()
-    hap1_12_qnames = list()
-    hap1_13_qnames = list()
-    hap1_14_qnames = list()
-    hap1_15_qnames = list()
-    hap1_16_qnames = list()
-    hap1_17_qnames = list()
-    hap1_18_qnames = list()
-    hap1_19_qnames = list()
-    hap1_20_qnames = list()
-    hap1_21_qnames = list()
-    hap1_22_qnames = list()
-    hap1_X_qnames = list()
-    hap1_Y_qnames = list()
-    hap2_1_qnames = list()
-    hap2_2_qnames = list()
-    hap2_3_qnames = list()
-    hap2_4_qnames = list()
-    hap2_5_qnames = list()
-    hap2_6_qnames = list()
-    hap2_7_qnames = list()
-    hap2_8_qnames = list()
-    hap2_9_qnames = list()
-    hap2_10_qnames = list()
-    hap2_11_qnames = list()
-    hap2_12_qnames = list()
-    hap2_13_qnames = list()
-    hap2_14_qnames = list()
-    hap2_15_qnames = list()
-    hap2_16_qnames = list()
-    hap2_17_qnames = list()
-    hap2_18_qnames = list()
-    hap2_19_qnames = list()
-    hap2_20_qnames = list()
-    hap2_21_qnames = list()
-    hap2_22_qnames = list()
-    hap2_X_qnames = list()
-    hap2_Y_qnames = list()
+    hap1_1_qnames = {}
+    hap1_2_qnames = {}
+    hap1_3_qnames = {}
+    hap1_4_qnames = {}
+    hap1_5_qnames = {}
+    hap1_6_qnames = {}
+    hap1_7_qnames = {}
+    hap1_8_qnames = {}
+    hap1_9_qnames = {}
+    hap1_10_qnames = {}
+    hap1_11_qnames = {}
+    hap1_12_qnames = {}
+    hap1_13_qnames = {}
+    hap1_14_qnames = {}
+    hap1_15_qnames = {}
+    hap1_16_qnames = {}
+    hap1_17_qnames = {}
+    hap1_18_qnames = {}
+    hap1_19_qnames = {}
+    hap1_20_qnames = {}
+    hap1_21_qnames = {}
+    hap1_22_qnames = {}
+    hap1_X_qnames = {}
+    hap1_Y_qnames = {}
+    hap2_1_qnames = {}
+    hap2_2_qnames = {}
+    hap2_3_qnames = {}
+    hap2_4_qnames = {}
+    hap2_5_qnames = {}
+    hap2_6_qnames = {}
+    hap2_7_qnames = {}
+    hap2_8_qnames = {}
+    hap2_9_qnames = {}
+    hap2_10_qnames = {}
+    hap2_11_qnames = {}
+    hap2_12_qnames = {}
+    hap2_13_qnames = {}
+    hap2_14_qnames = {}
+    hap2_15_qnames = {}
+    hap2_16_qnames = {}
+    hap2_17_qnames = {}
+    hap2_18_qnames = {}
+    hap2_19_qnames = {}
+    hap2_20_qnames = {}
+    hap2_21_qnames = {}
+    hap2_22_qnames = {}
+    hap2_X_qnames = {}
+    hap2_Y_qnames = {}
     
     with pysam.AlignmentFile(hap_bampath,'rb') as hap_bam :
       
@@ -78,102 +79,102 @@ def add_haplotypes(hap_bampath, me_bampath, out_bampath) :
             
             if hap == 1 :
                 if contig_n == '1':
-                    hap1_1_qnames.append(qname)
+                    hap1_1_qnames[qname] = 1
                 if contig_n == '2':
-                    hap1_2_qnames.append(qname)
+                    hap1_2_qnames[qname] = 1
                 if contig_n == '3':
-                    hap1_3_qnames.append(qname)
+                    hap1_3_qnames[qname] = 1
                 if contig_n == '4':
-                    hap1_4_qnames.append(qname)
+                    hap1_4_qnames[qname] = 1
                 if contig_n == '5':
-                    hap1_5_qnames.append(qname)
+                    hap1_5_qnames[qname] = 1
                 if contig_n == '6':
-                    hap1_6_qnames.append(qname)
+                    hap1_6_qnames[qname] = 1
                 if contig_n == '7':
-                    hap1_7_qnames.append(qname)
+                    hap1_7_qnames[qname] = 1
                 if contig_n == '8':
-                    hap1_8_qnames.append(qname)
+                    hap1_8_qnames[qname] = 1
                 if contig_n == '9':
-                    hap1_9_qnames.append(qname)
+                    hap1_9_qnames[qname] = 1
                 if contig_n == '10':
-                    hap1_10_qnames.append(qname)
+                    hap1_10_qnames[qname] = 1
                 if contig_n == '11':
-                    hap1_11_qnames.append(qname)
+                    hap1_11_qnames[qname] = 1
                 if contig_n == '12':
-                    hap1_12_qnames.append(qname)
+                    hap1_12_qnames[qname] = 1
                 if contig_n == '13':
-                    hap1_13_qnames.append(qname)                    
+                    hap1_13_qnames[qname] = 1                   
                 if contig_n == '14':
-                    hap1_14_qnames.append(qname)
+                    hap1_14_qnames[qname] = 1
                 if contig_n == '15':
-                    hap1_15_qnames.append(qname)
+                    hap1_15_qnames[qname] = 1
                 if contig_n == '16':
-                    hap1_16_qnames.append(qname)
+                    hap1_16_qnames[qname] = 1
                 if contig_n == '17':
-                    hap1_17_qnames.append(qname)
+                    hap1_17_qnames[qname] = 1
                 if contig_n == '18':
-                    hap1_18_qnames.append(qname)
+                    hap1_18_qnames[qname] = 1
                 if contig_n == '19':
-                    hap1_19_qnames.append(qname)
+                    hap1_19_qnames[qname] = 1
                 if contig_n == '20':
-                    hap1_20_qnames.append(qname)                    
+                    hap1_20_qnames[qname] = 1                    
                 if contig_n == '21':
-                    hap1_21_qnames.append(qname)
+                    hap1_21_qnames[qname] = 1
                 if contig_n == '22':
-                    hap1_22_qnames.append(qname)
+                    hap1_22_qnames[qname] = 1
                 if contig_n == 'X':
-                    hap1_X_qnames.append(qname)  
+                    hap1_X_qnames[qname] = 1
                 if contig_n == 'Y':
-                    hap1_Y_qnames.append(qname)                      
+                    hap1_Y_qnames[qname] = 1                      
             elif hap == 2 :
                 if contig_n == '1':
-                    hap2_1_qnames.append(qname)
+                    hap2_1_qnames[qname] = 2
                 if contig_n == '2':
-                    hap2_2_qnames.append(qname)
+                    hap2_2_qnames[qname] = 2
                 if contig_n == '3':
-                    hap2_3_qnames.append(qname)
+                    hap2_3_qnames[qname] = 2
                 if contig_n == '4':
-                    hap2_4_qnames.append(qname)
+                    hap2_4_qnames[qname] = 2
                 if contig_n == '5':
-                    hap2_5_qnames.append(qname)
+                    hap2_5_qnames[qname] = 2
                 if contig_n == '6':
-                    hap2_6_qnames.append(qname)
+                    hap2_6_qnames[qname] = 2
                 if contig_n == '7':
-                    hap2_7_qnames.append(qname)
+                    hap2_7_qnames[qname] = 2
                 if contig_n == '8':
-                    hap2_8_qnames.append(qname)
+                    hap2_8_qnames[qname] = 2
                 if contig_n == '9':
-                    hap2_9_qnames.append(qname)
+                    hap2_9_qnames[qname] = 2
                 if contig_n == '10':
-                    hap2_10_qnames.append(qname)
+                    hap2_10_qnames[qname] = 2
                 if contig_n == '11':
-                    hap2_11_qnames.append(qname)
+                    hap2_11_qnames[qname] = 2
                 if contig_n == '12':
-                    hap2_12_qnames.append(qname)
+                    hap2_12_qnames[qname] = 2
                 if contig_n == '13':
-                    hap2_13_qnames.append(qname)                    
+                    hap2_13_qnames[qname] = 2                   
                 if contig_n == '14':
-                    hap2_14_qnames.append(qname)
+                    hap2_14_qnames[qname] = 2
                 if contig_n == '15':
-                    hap2_15_qnames.append(qname)
+                    hap2_15_qnames[qname] = 2
                 if contig_n == '16':
-                    hap2_16_qnames.append(qname)
+                    hap2_16_qnames[qname] = 2
                 if contig_n == '17':
-                    hap2_17_qnames.append(qname)
+                    hap2_17_qnames[qname] = 2
                 if contig_n == '18':
-                    hap2_18_qnames.append(qname)
+                    hap2_18_qnames[qname] = 2
                 if contig_n == '19':
-                    hap2_19_qnames.append(qname)
+                    hap2_19_qnames[qname] = 2
                 if contig_n == '20':
-                    hap2_20_qnames.append(qname)                    
+                    hap2_20_qnames[qname] = 2                   
                 if contig_n == '21':
-                    hap2_21_qnames.append(qname)
+                    hap2_21_qnames[qname] = 2
                 if contig_n == '22':
-                    hap2_22_qnames.append(qname)
+                    hap2_22_qnames[qname] = 2
                 if contig_n == 'X':
-                    hap2_X_qnames.append(qname)  
+                    hap2_X_qnames[qname] = 2 
                 if contig_n == 'Y':
-                    hap2_Y_qnames.append(qname)      
+                    hap2_Y_qnames[qname] = 2      
 
     with pysam.AlignmentFile(me_bampath,'rb') as me_bam:
         f = pysam.AlignmentFile(out_bampath, 'wb',template = me_bam)
